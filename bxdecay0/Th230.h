99e7b3a0a0e09886f2f9b0ebbedf759fdc42cd0b
Pierre Lasorak <pierre.lasorak@gmail.com>
2020-07-17 08:01:24 -0500

    add th230

---
+++ bxdecay0/Th230.h
@@ -0,0 +1,20 @@
+#ifndef BXDECAY0_TH230_H
+#define BXDECAY0_TH230_H
+
+namespace bxdecay0 {
+
+  class i_random;
+  class event;
+
+  void Th230(i_random & prng_,
+             event & event_,
+             const double tcnuc_,
+             double & tdnuc_);
+
+} // end of namespace bxdecay0
+
+#endif // BXDECAY0_TH230_H
+
+// Local Variables: --
+// mode: c++ --
+// End: --
